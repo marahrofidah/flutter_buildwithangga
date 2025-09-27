1600502741908e0f5318d7b156b700c96a66fa3f
marahrofidah <marahrofidah7@gmail.com>
2025-09-27 22:01:09 +0700

    coba

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'codeinwithfida',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 179, 10, 10),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("codeinwithfida")),
+        body: SafeArea(child: Text("flutter pertama")),
+      ),
+    );
+  }
+}
