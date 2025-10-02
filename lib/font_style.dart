a96f1aa97a3104f1b9f0e4ff7c6faa4bd8147278
marahrofidah <marahrofidah7@gmail.com>
2025-10-02 11:53:33 +0700

    semibold

---
+++ lib/font_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle mainHeader = TextStyle(
+  fontSize: 26,
+  color: Colors.black,
+  fontFamily: 'Poppins',
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imrithys_rhymes/font_style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,18 +30,18 @@ class MyApp extends StatelessWidget {
         // blm bljr
         body: SafeArea(
           child: Container(
-            color: const Color.fromARGB(255, 208, 208, 208),
+            // color: const Color.fromARGB(255, 255, 255, 255),
             // margin: EdgeInsets.all(20),
             // margin: EdgeInsets.only(left: 10, top: 0, right: 0, bottom: 0),
-            padding: EdgeInsets.only(left: 0, top: 50, right: 0, bottom: 0),
+            padding: EdgeInsets.only(left: 100, top: 50, right: 0, bottom: 0),
             child: Column(
               children: <Widget>[
                 Image(
                   image: AssetImage('assets/images/gambar.png'),
                   height: 200,
                 ),
-                Text("Hi, I'm Fida"),
-                Text("Let's Connect yuhuuuu."),
+                Text("Hi, I'm Fida", style: mainHeader),
+                Text("Let's Connect yuhuuuu awwwwww."),
               ],
             ),
           ),
